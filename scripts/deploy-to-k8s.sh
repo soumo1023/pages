2fb6cd4f885fd01a0445e3730749bad88054e624
stifflerisboss <soumo1023@gmail.com>
2020-09-15 09:48:09 +0100

    db changes

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -2,12 +2,14 @@
 set -ex
 
 kubectl apply -f deployment/pages-namespace.yaml
+kubectl apply -f deployment/mysql-secret.yaml
+kubectl apply -f deployment/mysql-service.yaml
+kubectl apply -f deployment/mysql-deployment.yaml
+sleep 30
+kubectl apply -f deployment/flyway-configmap.yaml
+kubectl apply -f deployment/flyway-job.yaml
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
 kubectl apply -f deployment/pages-deployment.yaml
 
-kubectl apply -f deployment/mysql-secret.yaml
-kubectl apply -f deployment/mysql-service.yaml
-kubectl apply -f deployment/mysql-deployment.yaml
-kubectl apply -f deployment/flyway-configmap.yaml
-kubectl apply -f deployment/flyway-job.yaml
+
