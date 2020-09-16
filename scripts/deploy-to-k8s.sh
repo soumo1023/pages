107bcea735cebf1cdbc108846c8916414d0da617
stifflerisboss <soumo1023@gmail.com>
2020-09-16 08:15:21 +0100

    scaling

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -2,11 +2,16 @@
 set -ex
 
 kubectl apply -f deployment/pages-namespace.yaml
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
 kubectl apply -f deployment/mysql-secret.yaml
 kubectl apply -f deployment/mysql-service.yaml
+kubectl delete -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
+sleep 30
 kubectl apply -f deployment/flyway-configmap.yaml
 
+kubectl delete -f deployment/flyway-job.yaml
 kubectl apply -f deployment/flyway-job.yaml
 kubectl apply -f deployment/pages-config.yaml
 kubectl delete -f deployment/pages-service.yaml
